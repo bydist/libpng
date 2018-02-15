ec16b84b7bd98a5ca0f0da835308e7ffa588fd6b
David Callu <callu.david@gmail.com>
2018-02-15 09:44:00 +0100

    feat(version): bump to 1.6.28

--- id.cmake
+++ id.cmake
@@ -1,5 +1,5 @@
 set(package libpng)
-set(version 1.6.28)
+set(version 1.6.34)
 
 byd__package__info(${package}
     MAINTAINER_NAME "David Callu"
@@ -9,6 +9,6 @@ byd__package__info(${package}
     )
 
 byd__package__download_info(${package}
-    URL "http://prdownloads.sourceforge.net/libpng/libpng-1.6.28.tar.xz"
-    URL_HASH SHA1=ff4dceadb15e2c929ad26283118d56f66f4a6cff
+    URL "http://prdownloads.sourceforge.net/libpng/libpng-1.6.34.tar.xz"
+    URL_HASH SHA1=45de4ec996ffcc3e18037e7c128abe95f4d0292a
 )
