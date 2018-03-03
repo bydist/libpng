c38e1055fa98ec8b9a6642341b6e40771f9c78d4
David Callu <callu.david@gmail.com>
2018-03-03 19:46:39 +0100

    feat(id): use source from github

--- id.cmake
+++ id.cmake
@@ -4,11 +4,11 @@ set(version 1.6.34)
 byd__package__info(${package}
     MAINTAINER_NAME "David Callu"
     MAINTAINER_EMAIL "callu.david@gmail.com"
-    VERSION ${version}-1
+    VERSION ${version}-2
     ABI ${version}
     )
 
 byd__package__download_info(${package}
-    URL "http://prdownloads.sourceforge.net/libpng/libpng-1.6.34.tar.xz"
-    URL_HASH SHA1=45de4ec996ffcc3e18037e7c128abe95f4d0292a
+    URL "https://github.com/glennrp/libpng/archive/v1.6.28.tar.gz"
+    URL_HASH SHA1=776de185915b3780f07f207874615cf423e16d6d
 )
