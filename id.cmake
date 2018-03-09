ce95858d5c0a0f87c4368b51cec0226611789576
David Callu <callu.david@gmail.com>
2018-03-09 20:05:47 +0100

    fix(id): update to version 1.6.34-3 and add patch to build in debug mode

--- id.cmake
+++ id.cmake
@@ -4,11 +4,11 @@ set(version 1.6.34)
 byd__package__info(${package}
     MAINTAINER_NAME "David Callu"
     MAINTAINER_EMAIL "callu.david@gmail.com"
-    VERSION ${version}-2
+    VERSION ${version}-3
     ABI ${version}
     )
 
 byd__package__download_info(${package}
-    URL "https://github.com/glennrp/libpng/archive/v1.6.28.tar.gz"
-    URL_HASH SHA1=776de185915b3780f07f207874615cf423e16d6d
+    URL "https://github.com/glennrp/libpng/archive/v1.6.34.tar.gz"
+    URL_HASH SHA1=7d7a311c5c4ad6dbe87497764bf5d8fd0d802f0a
 )
